27e35255c036a343bbdc6c17300aca59739a0ccc
Oxtay <okhtay@gmail.com>
2014-03-17 19:44:39 -0700

    Version at the end of lesson 3

--- asciichan.py
+++ asciichan.py
@@ -14,19 +14,29 @@ class Handler(webapp2.RequestHandler):
         return t.render(params)
     def render(self, template, **kw):
         self.write(self.render_str(template, **kw))
+        
+class Art(db.Model):
+    title = db.StringProperty(required = True)
+    art = db.TextProperty(required = True)
+    created = db.DateTimeProperty(auto_now_add = True)
 
 class MainPage(Handler):
     def render_front(self, title="", art="", error=""):
+        arts = db.GqlQuery("SELECT * FROM Art ORDER BY created DESC")
         self.render("front.html", title=title, art=art, error = error)
+        
     def get(self):
         self.render_front()
+        
     def post(self):
         title = self.request.get("title")
         art = self.request.get("art")
         if title and art:
-            self.write("thanks!")
+            a = Art(title = title, art = art)
+            a.put()
+            self.redirect("/")
         else:
             error = "we need both a title and some artwork!"
-            self.render_front(error = error)
+            self.render_front(title, art, error)
 
 app = webapp2.WSGIApplication([('/', MainPage)], debug=True)
